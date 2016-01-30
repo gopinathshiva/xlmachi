e4daddd0129da3d50580050fa117d4b2fadae666
unknown <sgopinath31@gmail.com>
2016-01-30 10:09:41 +0530

    added slug removal for store

--- couponxl-child/functions.php
+++ couponxl-child/functions.php
@@ -602,4 +602,58 @@ function add_google_analytics() { ?>
  
 <?php }
 
+
+
+/**
+ * Remove the slug from published post permalinks. Only affect our custom post type, though.
+ */
+function gp_remove_cpt_slug( $post_link, $post, $leavename ) {
+ 
+    if ( 'store' != $post->post_type || 'publish' != $post->post_status ) {
+        return $post_link;
+    }
+ 
+    $post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+ 
+    return $post_link;
+}
+add_filter( 'post_type_link', 'gp_remove_cpt_slug', 10, 3 );
+
+function gp_parse_request_trick( $query ) {
+ 
+    // Only noop the main query
+    if ( ! $query->is_main_query() )
+        return;
+ 
+    // Only noop our very specific rewrite rule match
+    if ( 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+        return;
+    }
+ 
+    // 'name' will be set if post permalinks are just post_name, otherwise the page rule will match
+    if ( ! empty( $query->query['name'] ) ) {
+        $query->set( 'post_type', array( 'post', 'page', 'store' ) );
+    }
+}
+add_action( 'pre_get_posts', 'gp_parse_request_trick' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
