3c3c75e3946156488e28a93df8b1d285d621dbff
Gopinath Shiva <sgopinath31@gmail.com>
2016-03-28 08:54:20 +0530

    Update functions.php
    Hide the non-essential WordPress RSS Feeds

--- couponxl-child/functions.php
+++ couponxl-child/functions.php
@@ -642,6 +642,10 @@ remove_action( 'wp_head', 'wp_generator' ) ;
 remove_action( 'wp_head', 'wlwmanifest_link' ) ;
 remove_action( 'wp_head', 'rsd_link' ) ;
 
+// Hide the non-essential WordPress RSS Feeds
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+
 
 
 
